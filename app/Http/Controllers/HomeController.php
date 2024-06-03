c26d98443c1c303bcbc752abd0fe0e0d81ae3bc9
Beslan <64842276+Beslan066@users.noreply.github.com>
2024-06-03 22:44:26 +0300

    добавил переводы на страницу готовых переводов2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,17 +36,22 @@ class HomeController extends Controller
     public function completedSentences()
     {
 
+
+        $sentences = Sentence::all();
         $sentencesTranslate = Sentence::query()->where('status', 1)->orderBy('id', 'desc')->paginate(10);
+        $sentencesTranslateCompleted = Sentence::query()->where('status', 2)->orderBy('id', 'desc')->paginate(10);
+
+        $users = User::query()->where('role', 3)->get();
+
 
         $translates = [];
 
         foreach ($sentencesTranslate as $translate) {
             $translates = Translate::query()->where('sentence_id', $translate->id)->get();
         }
-        $sentencesTranslateCompleted = Sentence::query()->where('status', 2)->orderBy('id', 'desc')->paginate(10);
 
 
-        return view('sentences.completed', compact('sentencesTranslateCompleted', 'translates'));
+        return view('sentences.completed', compact('sentencesTranslateCompleted', 'translates', 'sentences', 'users'));
     }
 
     public function search(Request $request)
